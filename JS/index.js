7b900d3c9ebbc4e28b66a945f4e5efa7bb23f64c
Prabhu <prabhudattagiri34@gmail.com>
2023-10-08 19:31:25 +0530

    second

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+// alert("This is a pop up message");
+// var choice=confirm("Are you sure to exit");
+// document.write("Your Choice is ",choice)
+// It is not a good choice to use document.write
+let user = prompt("Enter Your Name : ");
+// document.write("Welcome Mr/Miss ",user);
+console.log("Welcome",user);
